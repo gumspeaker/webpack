ff2d556657d43f9342a8eb820db3bc998df3345a
gumspeaker <774910945@qq.com>
2023-03-15 23:34:08 +0800

    11

---
+++ config/swc-build.mjs
@@ -0,0 +1,17 @@
+import swc from "@swc/core";
+import fs from "fs";
+import { resolve } from "path";
+import path from "./path.js";
+const config = fs.readFileSync(resolve(path.appPath, ".swcrc"));
+const configObj = JSON.parse(config);
+swc
+  .transform(resolve(path.appSrc), {
+    // Some options cannot be specified in .swcrc
+    // filename: "input.js",
+    outputPath: resolve(path.appDirectory, "esm"),
+    ...configObj,
+  })
+  .then((output) => {
+    output.code; // transformed code
+    output.map; // source map (in string)
+  });
